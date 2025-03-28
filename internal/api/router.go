9793a98bc957470803f5e2df96e539deb2d83422
ke.gerlach <ke.gerlach@goreply.de>
2025-03-28 09:51:33 +0100

    Implemented the jwks endpoint to list signing keys

--- internal/api/router.go
+++ internal/api/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	health "simple-oauth2-server/internal/api/health"
+	"simple-oauth2-server/internal/api/jwks"
 	"simple-oauth2-server/internal/api/token"
 
 	"github.com/go-chi/chi"
@@ -14,6 +15,8 @@ func New() (*chi.Mux, error) {
 	r.Get("/health", health.Get)
 
 	r.Post("/token", token.Post)
+
+	r.Get("/jwks", jwks.Get)
 
 	return r, nil
 }
